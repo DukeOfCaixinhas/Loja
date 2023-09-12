5366fdc5052c266dc27980cd5817562805aab767
Duke <guidestrocastro@outlook.com>
2023-09-12 08:17:53 -0300

    Criação do package desconto & object CalculadoraDeDescontos

--- src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
+++ src/br/com/alura/loja/imposto/CalculadoraDeImpostos.java
@@ -8,6 +8,6 @@ public class CalculadoraDeImpostos {
 
     public BigDecimal calcular(Orcamento  orcamento, Imposto imposto){
         return imposto.calcular(orcamento);
-
     }
+
 }
--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -4,12 +4,18 @@ import java.math.BigDecimal;
 
 public class Orcamento {
     private BigDecimal valor;
+    private int quantidadeItens;
 
     public BigDecimal getValor() {
         return valor;
     }
 
-    public Orcamento(BigDecimal valor) {
+    public Orcamento(BigDecimal valor, int quantidadeItens ) {
         this.valor = valor;
+        this.quantidadeItens = quantidadeItens;
+    }
+
+    public int getQuantidadeItens() {
+        return quantidadeItens;
     }
 }
