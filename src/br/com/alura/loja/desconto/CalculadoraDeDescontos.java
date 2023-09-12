fc7a824f3a2e8b6f444d1ca948a06e4ca9ce1557
Duke <guidestrocastro@outlook.com>
2023-09-12 08:36:26 -0300

    Criação de dois objects de Desconto no package desconto

--- src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
+++ src/br/com/alura/loja/desconto/CalculadoraDeDescontos.java
@@ -5,14 +5,12 @@ import br.com.alura.loja.orcamento.Orcamento;
 import java.math.BigDecimal;
 
 public class CalculadoraDeDescontos {
-    public BigDecimal calcular(Orcamento orcamento){
-        if (orcamento.getQuantidadeItens() > 5){
-            return orcamento.getValor().multiply(new BigDecimal("0.1"));
-        }
-
-        if (orcamento.getValor().compareTo(new BigDecimal("500")) > 0){
-            return orcamento.getValor().multiply(new BigDecimal("0.1"));
-        }
-        return BigDecimal.ZERO;
+    public BigDecimal calcular(Orcamento orcamento) {
+    BigDecimal desconto = new DescontoParaOrcamentoComMaisDeCincoItens().calcular(orcamento);
+    if (desconto == BigDecimal.ZERO) {
+        desconto = new DescontoParaOrcamentoComValorMaiorQueQuinhentos().calcular(orcamento);
     }
+    return BigDecimal.ZERO;
+    }
+
 }
