d5f4e6b9b6c4ec79379cc57f41ec0b26e5090812
Duke <guidestrocastro@outlook.com>
2023-09-13 08:50:22 -0300

    Função aplicarDescontoExtra

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -5,9 +5,23 @@ import java.math.BigDecimal;
 public class Orcamento {
     private BigDecimal valor;
     private int quantidadeItens;
+    private String situacao;
 
     public BigDecimal getValor() {
         return valor;
+    }
+    public void aplicarDescontoExtra(){
+        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+        if(situacao.equals("EM ANALISE")){
+            valorDoDescontoExtra = new BigDecimal("0.05");
+        } else if (situacao.equals("APROVADO")) {
+            valorDoDescontoExtra = new BigDecimal("0.02");
+            
+        }
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+    public void aprovar() {
+        this.situacao = "APROVADO";
     }
 
     public Orcamento(BigDecimal valor, int quantidadeItens ) {
